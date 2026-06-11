5ed5d53e1497d0d4bf159f5df4e6f8f13bdaec45
mahmoud alhajj <mahmoud.alhajj@et3arraf.com>
2026-06-11 14:21:54 +0300

    Initial ET3 designer library setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## Steps to recreate:
+
+npm i typescript
+npm i tsup
+
+package.json
+manually created tsup.config.ts which through the tsup libraray creates the needed configuration for it to be an npm library.
---
+++ src/colors.ts
@@ -0,0 +1,26 @@
+export enum Colors {
+    PRIMARY = "#0055FF",
+    PRIMARY_LIGHT = "#4D88FF",
+    PRIMARY_DARK = "#003BB5",
+
+    SECONDARY = "#FF7A00",
+
+    SUCCESS = "#22C55E",
+    WARNING = "#F59E0B",
+    ERROR = "#EF4444",
+    INFO = "#3B82F6",
+
+    WHITE = "#FFFFFF",
+    BLACK = "#000000",
+
+    GREY_50 = "#F9FAFB",
+    GREY_100 = "#F3F4F6",
+    GREY_200 = "#E5E7EB",
+    GREY_300 = "#D1D5DB",
+    GREY_400 = "#9CA3AF",
+    GREY_500 = "#6B7280",
+    GREY_600 = "#4B5563",
+    GREY_700 = "#374151",
+    GREY_800 = "#1F2937",
+    GREY_900 = "#111827",
+}
